6a10521200c3d86e26370d491cc88b93f1017af0
Alexander Ozero <a.ozero@live.com>
2020-10-03 12:54:50 -0600

    Add basic robot

---
+++ Treat Bomber/Assets/Scripts/Robot.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class Robot : LittleBeast
+{
+    private Vector2 speed = new Vector2(0.1f, 0);
+    protected Vector2 movement = new Vector2();
+
+    protected Rigidbody2D rigidBody = null;
+    protected SpriteRenderer sprite = null;
+
+    float lurchTime = 1.0f;
+    float timeLeft = 0;
+    bool moving = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preferredCandyType = eCandyType.ROBOT;
+
+        rigidBody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        // Randomly start moving to the left or right.
+        int[] values = { -1, 1 };
+        movement.x = values[Random.Range(0, 2)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0.0f)
+        {
+            timeLeft = lurchTime;
+            moving = !moving;
+        }
+        if (moving)
+        {
+            DoMove();
+            FlipToMovement();
+        }
+    }
+
+    private void DoMove()
+    {
+        rigidBody.MovePosition(rigidBody.position + (movement * speed));
+    }
+
+    // If going right, flip sprite
+    // If going left, unflip sprite
+    // If not moving, do not change spirte
+    private void FlipToMovement()
+    {
+        if (movement.x > 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (movement.x < 0)
+        {
+            sprite.flipX = false;
+        }
+    }
+
+    // On colliding with a wall
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Change movement direction
+        movement.x *= -1;
+    }
+}
