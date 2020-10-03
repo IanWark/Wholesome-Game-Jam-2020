2b57790747c77612b78298d9da2857b80e05715c
Alexander Ozero <a.ozero@live.com>
2020-10-03 17:42:34 -0600

    Add jumping to scarecrow

--- Treat Bomber/Assets/Scripts/Scarecrow.cs
+++ Treat Bomber/Assets/Scripts/Scarecrow.cs
@@ -2,13 +2,20 @@
 
 public class Scarecrow : LittleBeast
 {
-    private Vector2 speed = new Vector2(0.05f, 0);
-    protected Vector2 movement = new Vector2();
-
     protected Rigidbody2D rigidBody = null;
     protected SpriteRenderer sprite = null;
 
+    private Vector2 xSpeed = new Vector2(0.125f, 0);
+    protected Vector2 movement = new Vector2();
+
+    private float ySpeed;
+    private float jumpSpeed = 0.05f;
+    private float acceleration = -0.005f; // Gravity
     
+    private Vector2 height = new Vector2(0, 0);
+    private Vector2 groundHeight = new Vector2();
+    private Vector2 zeroY = new Vector2(1, 0); // Help snap to ground height
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +28,36 @@ public class Scarecrow : LittleBeast
         // Randomly start moving to the left or right.
         int[] values = { -1, 1 };
         movement.x = values[Random.Range(0, 2)];
+
+        // Set the starting ground height to where the scarecrow is standing
+        groundHeight.x = 0;
+        groundHeight.y = rigidBody.position.y;
+
+        ySpeed = jumpSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Accelerate the jump/fall
+        height.y += ySpeed + acceleration / 2;
+        ySpeed += acceleration;
+        
         DoMove();
         FlipToMovement();
+
+        // If hitting the ground, start jumping again
+        if (rigidBody.position.y < groundHeight.y)
+        {
+            height.y = 0;
+            ySpeed = jumpSpeed;
+            rigidBody.MovePosition((rigidBody.position * zeroY) + groundHeight);
+        }
     }
 
     private void DoMove()
     {
-        rigidBody.MovePosition(rigidBody.position + (movement * speed));
+        rigidBody.MovePosition(rigidBody.position + (movement * xSpeed) + height);
     }
 
     // If going right, flip sprite
