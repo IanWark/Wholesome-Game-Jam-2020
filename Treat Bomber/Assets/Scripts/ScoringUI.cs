c81565d2c5fd2a7ef1406f2e85fa0c97b6a20d52
IanWark <wark@ualberta.ca>
2020-10-03 17:03:02 -0400

    Add score and strikes UI

---
+++ Treat Bomber/Assets/Scripts/ScoringUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoringUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI scoreValue = null;
+
+    [SerializeField]
+    private TextMeshProUGUI strikesValue = null;
+    [SerializeField]
+    private TextMeshProUGUI strikesMax = null;
+
+    public void SetScoreValue(int value)
+    {
+        scoreValue.text = value.ToString();
+    }
+
+    public void SetStrikesValue(int value)
+    {
+        strikesValue.text = value.ToString();
+    }
+
+    public void SetStrikesMax(int value)
+    {
+        strikesMax.text = value.ToString();
+    }
+
+}
