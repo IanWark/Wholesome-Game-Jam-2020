b6d8b83353c0266f56fef5a6059bda326d2b9808
IanWark <wark@ualberta.ca>
2020-10-04 02:11:50 -0400

    Fix vampires not always spawning up

--- Treat Bomber/Assets/Scripts/Little Beasts/Vampire.cs
+++ Treat Bomber/Assets/Scripts/Little Beasts/Vampire.cs
@@ -5,9 +5,14 @@ public class Vampire : LittleBeast
     private Vector2 speed = new Vector2(0.15f, 0);
     protected float leavingSpeedMultiplier = 5;
 
-    private Vector2 raiseVampire = new Vector2(1, 0.25f);
+    static public Vector2 raiseVampire = new Vector2(1, 0.25f);
     private Vector2 sinWave = new Vector2(0, 0);
     private float sinVal = 0;
+
+    private void Awake()
+    {
+        
+    }
 
     // Start is called before the first frame update
     protected override void Start()
@@ -17,7 +22,7 @@ public class Vampire : LittleBeast
         base.Start();
 
         // Spawn vampire above other monsters
-        rigidBody.MovePosition(rigidBody.position * raiseVampire);
+        rigidBody.position = rigidBody.position * raiseVampire;
     }
 
     // Update is called once per frame
