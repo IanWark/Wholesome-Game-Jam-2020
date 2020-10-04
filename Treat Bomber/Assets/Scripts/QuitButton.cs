0440d8dc9b50dde5e278a2dfba8f6eb31eaca4e8
IanWark <wark@ualberta.ca>
2020-10-04 13:50:51 -0400

    Add quit button to game over screen

---
+++ Treat Bomber/Assets/Scripts/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        // Reload scene
+        Application.Quit();
+    }
+}
